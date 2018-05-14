e6053ed1b14946aecc8500f02cb0c5670804b591
Cael Rowley <caelrowley@gmail.com>
2018-05-14 22:36:56 +0100

    Added account mappings for elastic

--- elastic/mappings/addMappings.sh
+++ elastic/mappings/addMappings.sh
@@ -1,6 +1,9 @@
 curl -XPUT http://localhost:9200/blocks
 curl -XPUT http://localhost:9200/issuedassets
+curl -XPUT http://localhost:9200/accounts
 
 curl -XPUT "http://localhost:9200/blocks/block/_mapping" -H'Content-Type: application/json' -d @blockMapping.json
 curl -XPUT "http://localhost:9200/issuedassets/issuedasset/_mapping" -H'Content-Type: application/json' -d @issuedAssetMapping.json
+curl -XPUT "http://localhost:9200/accounts/account/_mapping" -H'Content-Type: application/json' -d @accountMapping.json
+
 curl -XPUT "http://localhost:9200/nodes" -H 'Content-Type: application/json' -d @nodeMapping.json
